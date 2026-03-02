399f8ea388e774097dd57cff6dd6ff285a52797c
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-03-02 17:57:03 +0800

    update

--- JS/8. insert-element.js
+++ JS/8. insert-element.js
@@ -2,10 +2,13 @@ function insertElement() {
   const filter = document.querySelector(".filter");
   const h1 = document.createElement("h1");
   h1.textContent = "insertAdjacementElement";
+  //   filter.insertAdjacentElement("beforebegin", h1);
   filter.insertAdjacentElement("beforebegin", h1);
   const h1after = document.createElement("h1");
+  //   h1after.textContent = "insertAdjacementElement";
   h1after.textContent = "insertAdjacementElement";
   const filter1 = document.querySelector(".filter");
+  //   filter1.insertAdjacentElement("afterend", h1after);
   filter1.insertAdjacentElement("afterend", h1after);
 }
 insertElement();
