c9e4757271bdf00dc71a6d311e35dae71388e343
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-03-02 10:08:53 +0800

    update

--- JS/1. shopping-list.js
+++ JS/1. shopping-list.js
@@ -6,4 +6,33 @@ output = document.documentElement;
 output = document.head;
 output = document.body;
 output = document.body.children;
-console.log(output);
+//  other element types
+output = document.domain;
+output = document.URL;
+output = document.doctype;
+output = document.characterSet;
+output = document.contentType;
+// forms
+output = document.forms;
+output = document.forms[0];
+output = document.forms[0].id;
+output = document.forms[0].method;
+output = document.forms[0].action;
+document.forms[0].id = "new-id";
+output = document.forms[0].id;
+// links
+output = document.links;
+output = document.links[0];
+output = document.links[0].href;
+output = document.links[0].href = "https://facebook.com";
+output = document.links[0].className = "text-red";
+output = document.links[0].classList;
+// images
+output = document.images;
+output = document.images[0];
+output = document.images[0].src;
+// convert HTMLCollection to Array type and then forEach to loop through
+const forms = Array.from(document.forms);
+forms.forEach((form) => console.log(form));
+
+output = console.log(output);
