1ba8d1b55ef86c8770a57e0384f2ea7ddaf9313d
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-03-04 10:16:46 +0800

    update

--- JS/14. object-event.js
+++ JS/14. object-event.js
@@ -1,21 +1,26 @@
+// a. add event listener with callback function
 const logo = document.querySelector("header");
-logo.addEventListener("click", (e) => {
-  console.log(e);
-  console.log(e.currentTarget);
+logo.addEventListener("click", (event) => {
+  console.log(event);
+  console.log(event.currentTarget);
 });
-function onClick(e) {
-  console.log(e.target);
-  console.log(e.currentTarget);
-  e.target.style.backgroudColor = "red";
-  console.log(e.clientX);
-  console.log(e.clientY);
-  console.log(e.offsetX);
-  console.log(e.offsetY);
+// b. rewrite to callback function
+function onClick(event) {
+  console.log(event.target); //return element that trigger the event, in this case the img
+  console.log(event.currentTarget); //return element that the event listener is attached to, in this case is the container
+  event.target.style.backgroundColor = "red";
+  console.log(event.clientX); // absolute value from page position
+  console.log(event.clientY);
+  console.log(event.offsetX); //from element position
+  console.log(event.offsetY);
 }
-
 logo.addEventListener("click", onClick);
-
-document.querySelector("a").addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("link is clicked");
+// prevent default is to stop the link from functioning
+document.querySelector("a").addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(`link is clicked`);
 });
+function onDrag(e) {
+  document.querySelector("h1").textContent = `x:${e.clientX},y:${e.clientY}`;
+}
+logo.addEventListener("drag", onDrag);
