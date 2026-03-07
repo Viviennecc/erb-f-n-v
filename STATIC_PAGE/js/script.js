873ef4c2aa87774af048bbf613a738c037b765f9
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-03-07 11:01:46 +0800

    update

--- STATIC_PAGE/js/script.js
+++ STATIC_PAGE/js/script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
-  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-list");
   toggleButton.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
   });
-});
 
-const date = (document.getElementById("date").innerHTML =
-  new Date().getFullYear());
+  const date = (document.getElementById("date").innerHTML =
+    new Date().getFullYear());
+});
